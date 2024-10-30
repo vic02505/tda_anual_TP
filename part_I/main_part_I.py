109724258997fa868a4f7a28c448d166fb76212c
MateoLardiez <mlardiez@fi.uba.ar>
2024-10-30 15:22:06 -0300

    modifing coin_game function. Append of last coin was missing. Adding new example

--- part_I/main_part_I.py
+++ part_I/main_part_I.py
@@ -5,7 +5,7 @@ from libs import datasets_parser
 def coins_game(coins_list):
     
     
-    coins_list.sort(reverse=True)
+    #coins_list.sort(reverse=True)
     
     sophia_coins = []
     mateo_coins = []
@@ -19,7 +19,7 @@ def coins_game(coins_list):
         
         selected_coin = -1
         
-        if sophia_is_playing:   
+        if sophia_is_playing:
             if coins_list[beginning] < coins_list[end]:
                 selected_coin = coins_list[end]
                 end -=1
@@ -30,7 +30,6 @@ def coins_game(coins_list):
             sophia_coins.append(selected_coin)
             sophia_is_playing = False
         else:
-            
             if coins_list[beginning] > coins_list[end]:
                 selected_coin = coins_list[end]
                 end -=1
@@ -42,10 +41,39 @@ def coins_game(coins_list):
             sophia_is_playing = True      
     
     return (sophia_coins, mateo_coins)
+
+
+def coins_game_v2(coins_list):
+    sophia_coins = []
+    mateo_coins = []
     
     
-coins_list = datasets_parser.get_coins_list('20000.txt')
-sophia_coins, mateo_coins = coins_game(coins_list)
+    sophia_is_playing = True
+    while len(coins_list) > 0:
+        if sophia_is_playing:
+            # Tomo la moneda de mayor valor. De ser iguales tomo la del principio.
+            if coins_list[0] > coins_list[-1]:
+                selected_coin = coins_list.pop(0)
+            else:
+                selected_coin = coins_list.pop()
+                
+            sophia_coins.append(selected_coin)
+            sophia_is_playing = False
+        else:
+            # Tomo la moneda de menor valor. De ser iguales tomo la del principio.
+            if coins_list[0] > coins_list[-1]:
+                selected_coin = coins_list.pop()
+            else:
+                selected_coin = coins_list.pop(0)
+                
+            mateo_coins.append(selected_coin)
+            sophia_is_playing = True
+    
+    return (sophia_coins, mateo_coins)
+
+    
+coins_list = datasets_parser.get_coins_list('6.txt')
+sophia_coins, mateo_coins = coins_game_v2(coins_list)
 
 
 print(f"Sofia {sum(sophia_coins)}")
