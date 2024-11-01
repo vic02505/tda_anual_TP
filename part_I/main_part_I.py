76f2c2b6f53e7a243d25a216a50203fc1d1f22fb
MateoLardiez <mlardiez@fi.uba.ar>
2024-11-01 12:36:42 -0300

    fix en la funcion

--- part_I/main_part_I.py
+++ part_I/main_part_I.py
@@ -3,10 +3,6 @@ from libs import datasets_parser
 #Agregar parametro que indique si se quiere ordenar la lista o no. Esto no sirve para comparar
 #resultados.
 def coins_game(coins_list):
-    
-    
-    #coins_list.sort(reverse=True)
-    
     sophia_coins = []
     mateo_coins = []
     
@@ -15,8 +11,8 @@ def coins_game(coins_list):
     beginning = 0
     end = len(coins_list) - 1
     
-    while beginning < end:
-        
+    while beginning <= end:
+
         selected_coin = -1
         
         if sophia_is_playing:
@@ -42,38 +38,9 @@ def coins_game(coins_list):
     
     return (sophia_coins, mateo_coins)
 
-
-def coins_game_v2(coins_list):
-    sophia_coins = []
-    mateo_coins = []
-    
-    
-    sophia_is_playing = True
-    while len(coins_list) > 0:
-        if sophia_is_playing:
-            # Tomo la moneda de mayor valor. De ser iguales tomo la del principio.
-            if coins_list[0] > coins_list[-1]:
-                selected_coin = coins_list.pop(0)
-            else:
-                selected_coin = coins_list.pop()
-                
-            sophia_coins.append(selected_coin)
-            sophia_is_playing = False
-        else:
-            # Tomo la moneda de menor valor. De ser iguales tomo la del principio.
-            if coins_list[0] > coins_list[-1]:
-                selected_coin = coins_list.pop()
-            else:
-                selected_coin = coins_list.pop(0)
-                
-            mateo_coins.append(selected_coin)
-            sophia_is_playing = True
-    
-    return (sophia_coins, mateo_coins)
-
     
 coins_list = datasets_parser.get_coins_list('6.txt')
-sophia_coins, mateo_coins = coins_game_v2(coins_list)
+sophia_coins, mateo_coins = coins_game(coins_list)
 
 
 print(f"Sofia {sum(sophia_coins)}")
